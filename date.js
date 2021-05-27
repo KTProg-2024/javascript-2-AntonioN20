da15a0c8f22f1f8025f6e1e576146b2260ca67a6
Antonio Neto <>
2021-05-27 16:18:45 +0000

    Finished

--- date.js
+++ date.js
@@ -3,3 +3,13 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+
+function red()
+{
+  document.getElementById("date").style.color="red";
+}
+
+function purple()
+{
+  document.getElementById("date").style.color="purple";
+}
